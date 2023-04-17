1db336749b635be11e9e0c884c48affced6f9fe4
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2023-04-17 02:54:55 +0100

    Static Utils
    made the file utilities static calls
    Read/Write calls now static
    New FileComponent for fetching filepaths and making files also static calls

--- src/main/java/com/example/teamRl/Activity.java
+++ src/main/java/com/example/teamRl/Activity.java
@@ -3,4 +3,9 @@ package com.example.teamRl;
 // activity class is a POJO for activity data
 public class Activity {
 
+    private String name;
+    private String category;
+    private String description;
+    private String email;
+
 }
--- src/main/java/com/example/teamRl/HelloApplication.java
+++ src/main/java/com/example/teamRl/HelloApplication.java
@@ -11,21 +11,19 @@ import java.util.ArrayList;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registrationForm.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        ReadComponent RC = new ReadComponent();
-        ArrayList<User> users = RC.readUserDataFromJSONFile("Users.txt", "main");
+        DataGenerator DG = new DataGenerator();
+        ArrayList<User> users = ReadComponent.readUserDataFromJSON("Users.txt", "main");
 
-        for(int i = 0; i<users.size(); i++)
-        {
-            System.out.println(users.get(i).toJSON());
-        }
-        //launch();
+        for(int i = 0; i< users.size();i++){System.out.println(users.get(i).toJSON());}
+        System.out.println(users.size());
+
     }
 }
