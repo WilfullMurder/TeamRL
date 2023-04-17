de6cd501f6c71dc1e1b80283af98ac7c57c37d0a
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2023-04-17 02:47:17 +0100

    Updated Project files
    Making uilities static calls
    Moved some file utility functions into a new class FileComponent and made them static
    Made the public ReadComponent and WriteComponent methods static

--- src/test/java/com/example/teamRl/UserTest.java
+++ src/test/java/com/example/teamRl/UserTest.java
@@ -1,39 +1,41 @@
 package com.example.teamRl;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
     /**
      * Methods under test:
-     *
-     *   {@link User#User()}
-     *   {@link User#setAdmin(Admin)}
-     *   {@link User#setAdminFlag(boolean)}
-     *   {@link User#setDOB(String)}
-     *   {@link User#setEmail(String)}
-     *   {@link User#setEndYear(String)}
-     *   {@link User#setForename(String)}
-     *   {@link User#setPassword(String)}
-     *   {@link User#setStartYear(String)}
-     *   {@link User#setSuperFlag(boolean)}
-     *   {@link User#setSuperUser(SuperUser)}
-     *   {@link User#setSurname(String)}
-     *   {@link User#setUobNumber(String)}
-     *   {@link User#requestAdminRights()}
-     *   {@link User#getAdmin()}
-     *   {@link User#getAdminFlag()}
-     *   {@link User#getDOB()}
-     *   {@link User#getEmail()}
-     *   {@link User#getEndYear()}
-     *   {@link User#getForename()}
-     *   {@link User#getPassword()}
-     *   {@link User#getStartYear()}
-     *   {@link User#getSuperFlag()}
-     *   {@link User#getSuperUser()}
-     *   {@link User#getSurname()}
-     *   {@link User#getUobNumber()}
+     * <p>
+     * {@link User#User()}
+     * {@link User#setAdmin(Admin)}
+     * {@link User#setAdminFlag(boolean)}
+     * {@link User#setDOB(String)}
+     * {@link User#setEmail(String)}
+     * {@link User#setEndYear(String)}
+     * {@link User#setForename(String)}
+     * {@link User#setPassword(String)}
+     * {@link User#setStartYear(String)}
+     * {@link User#setSuperFlag(boolean)}
+     * {@link User#setSuperUser(SuperUser)}
+     * {@link User#setSurname(String)}
+     * {@link User#setUobNumber(String)}
+     * {@link User#requestAdminRights()}
+     * {@link User#getAdmin()}
+     * {@link User#getAdminFlag()}
+     * {@link User#getDOB()}
+     * {@link User#getEmail()}
+     * {@link User#getEndYear()}
+     * {@link User#getForename()}
+     * {@link User#getPassword()}
+     * {@link User#getStartYear()}
+     * {@link User#getSuperFlag()}
+     * {@link User#getSuperUser()}
+     * {@link User#getSurname()}
+     * {@link User#getUobNumber()}
      */
     @Test
     void testConstructor() {
@@ -69,33 +71,33 @@ class UserTest {
 
     /**
      * Methods under test:
-     *
-     *   {@link User#User(String, String)}
-     *   {@link User#setAdmin(Admin)}
-     *   {@link User#setAdminFlag(boolean)}
-     *   {@link User#setDOB(String)}
-     *   {@link User#setEmail(String)}
-     *   {@link User#setEndYear(String)}
-     *   {@link User#setForename(String)}
-     *   {@link User#setPassword(String)}
-     *   {@link User#setStartYear(String)}
-     *   {@link User#setSuperFlag(boolean)}
-     *   {@link User#setSuperUser(SuperUser)}
-     *   {@link User#setSurname(String)}
-     *   {@link User#setUobNumber(String)}
-     *   {@link User#requestAdminRights()}
-     *   {@link User#getAdmin()}
-     *   {@link User#getAdminFlag()}
-     *   {@link User#getDOB()}
-     *   {@link User#getEmail()}
-     *   {@link User#getEndYear()}
-     *   {@link User#getForename()}
-     *   {@link User#getPassword()}
-     *   {@link User#getStartYear()}
-     *   {@link User#getSuperFlag()}
-     *   {@link User#getSuperUser()}
-     *   {@link User#getSurname()}
-     *   {@link User#getUobNumber()}
+     * <p>
+     * {@link User#User(String, String)}
+     * {@link User#setAdmin(Admin)}
+     * {@link User#setAdminFlag(boolean)}
+     * {@link User#setDOB(String)}
+     * {@link User#setEmail(String)}
+     * {@link User#setEndYear(String)}
+     * {@link User#setForename(String)}
+     * {@link User#setPassword(String)}
+     * {@link User#setStartYear(String)}
+     * {@link User#setSuperFlag(boolean)}
+     * {@link User#setSuperUser(SuperUser)}
+     * {@link User#setSurname(String)}
+     * {@link User#setUobNumber(String)}
+     * {@link User#requestAdminRights()}
+     * {@link User#getAdmin()}
+     * {@link User#getAdminFlag()}
+     * {@link User#getDOB()}
+     * {@link User#getEmail()}
+     * {@link User#getEndYear()}
+     * {@link User#getForename()}
+     * {@link User#getPassword()}
+     * {@link User#getStartYear()}
+     * {@link User#getSuperFlag()}
+     * {@link User#getSuperUser()}
+     * {@link User#getSurname()}
+     * {@link User#getUobNumber()}
      */
     @Test
     void testConstructor2() {
@@ -145,6 +147,27 @@ class UserTest {
         assertEquals("DOB", actualUser.getDOB());
     }
 
+    /**
+     * Method under test: {@link User#User(String, String, String, String, String, String, String)}
+     */
+    @Test
+    @Disabled("TODO: Complete this test")
+    void testConstructor4() {
+        // TODO: Complete this test.
+        //   Reason: R013 No inputs found that don't throw a trivial exception.
+        //   Diffblue Cover tried to run the arrange/act section, but the method under
+        //   test threw
+        //   java.lang.StringIndexOutOfBoundsException: begin 0, end 2, length 0
+        //       at java.lang.String.checkBoundsBeginEnd(String.java:3319)
+        //       at java.lang.String.substring(String.java:1874)
+        //       at com.example.teamRl.User.checkForStaff(User.java:69)
+        //       at com.example.teamRl.User.<init>(User.java:63)
+        //   See https://diff.blue/R013 to resolve this issue.
+
+        new User("Doe", "Forename", "", "jane.doe@example.org", "DOB", "Start Year", "End Year");
+
+    }
+
 
     /**
      * Method under test: {@link User#toJSON()}
@@ -163,6 +186,7 @@ class UserTest {
     @Test
     void testToCSV() {
         assertEquals("null,null,null", (new User("foo", "foo")).toCSV());
+        assertEquals("null,null,null,foo,null,null,null", (new User("foo", "foo")).toCSV());
     }
 }
 
