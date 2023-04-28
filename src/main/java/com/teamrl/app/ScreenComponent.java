ad6f0da5fd78dc002acac523f3299fe6e33c321d
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2023-04-28 19:38:06 +0100

    Old file deletion
    Because it keeps the old files when I overwrite the project I am deleting them from the branch in hopes that will sort it out

--- src/main/java/com/teamrl/app/ScreenComponent.java
+++
@@ -1,9 +0,0 @@
-package com.teamrl.app;
-
-import javafx.stage.Screen;
-
-public class ScreenComponent {
-    public static final double SCREEN_WIDTH = Screen.getPrimary().getVisualBounds().getWidth();
-    public static final double SCREEN_HEIGHT = Screen.getPrimary().getVisualBounds().getHeight();
-
-}
